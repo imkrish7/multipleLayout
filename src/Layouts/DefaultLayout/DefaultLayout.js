e9c0f5d1ee7f332e59af1db905dd14f52f8f7d57
Rackham <singhkrishk07@gmail.com>
2020-10-21 20:02:11 +0530

    Console removed

--- src/Layouts/DefaultLayout/DefaultLayout.js
+++ src/Layouts/DefaultLayout/DefaultLayout.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
 import DefaultHeader from "./DefaultHeader";
 import DefaultFooter from "./DefaultFooter";
 import styles from "../../styles/defaultlayout.module.scss";
 class DefaultLayout extends Component {
-  componentDidMount() {
-    console.log(this.props);
-  }
   render() {
     return (
       <div className={styles.container}>
